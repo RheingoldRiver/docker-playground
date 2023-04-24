570fcc10d6be1f6566474735fc573b1562b4c302
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2023-04-24 01:59:02 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.udemy.com/course/docker-and-kubernetes-the-complete-guide/learn/lecture/18799500#overview"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Read about WSL & Docker Volumes on Windows
         </a>
+        <pre>docker run -p 3000:3000 -v /app/node_modules -v ${"{pwd}"}:/app frontend</pre>
+        This must be done in WINDOWS POWERSHELL, and not in WSL. Happy devops!! :)
+        <div class="list-wrapper">
+          <ul>
+            <li>
+              The first <code>-v</code> is a "bookmark" on <code>node_modules</code> because we deleted it in our local
+              directory, so Docker needs to use its own.
+            </li>
+            <li>
+              The second <code>-v</code> tells it to map the local <code>pwd</code> contents to the container's{" "}
+              <code>app/frontend</code> folder.
+            </li>
+            <li>
+              The contents of this command are coupled very heavily to the contents of the Dockerfile. For example, if
+              we had said something other than <code>WORKDIR /app</code>, then we would say something other than{" "}
+              <code>app/frontend</code> here.
+            </li>
+          </ul>
+        </div>
       </header>
     </div>
   );
